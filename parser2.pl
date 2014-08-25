1fa18c7e22f0bd37cebfedd9a40c6905ff1b4c28
James Hazard <james.g.hazard@gmail.com>
2014-08-25 15:48:33 -0400

    Initial Porting

---
+++ parser2.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -wl
+
+use warnings;
+use strict;
+
+my @missing;
+my $path = "./Doc/";
+my $orig;
+
+sub parse {
+	
+	my($file,$root,$part) = @_; my $f;
+
+	ref($file) eq "GLOB" ? $f = $file : open $f, $file or die $!;
+	$orig = $f unless $orig;
+	
+	my $content = parse_root($f, $root);
+	if($content) { expand_fields($f, \$content, $part); return($content) }
+
+	return;
+}
+
+
+sub parse_root { 
+	
+	my ($f, $field) = @_; my $root;
+
+	seek($f, 0, 0);	
+	while(<$f>) {
+		return $root if ($root) = $_ =~ /^\Q$field\E\s*=\s*(.*?)$/;
+	}
+	
+	
+	seek($f, 0, 0);
+	while(<$f>) {
+		my($part,$what);	
+		if( (($part, $what) = $_ =~ /^([^=]*)=\[(.+?)\]/) and ($field =~ s/^\Q$part\E//) ) {
+			$root = parse($path.$what, $field, $part);
+			return $root if $root;
+		}
+	}
+	return $root;
+
+} 
+
+sub expand_fields  {
+
+	my($f,$field,$part) = @_;
+
+	foreach( $$field =~ /\{([^}]+)\}/g ) {
+		my $ex = $_;
+		my $ox = $part ? $part . $ex : $ex;
+		my $value = parse($orig, $ox);
+		$value ? $$field =~ s/\{\Q$ex\E\}/$value/gg : push @missing, $ex;
+	}
+} 
+
+
+
+print " ".parse($ARGV[0], "Model.Root");
+# print "\nMissing elements: @missing";
