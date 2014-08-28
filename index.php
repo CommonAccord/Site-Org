0650ae835d5437a2ba26aeb3f58b9a6023f1460c
root <root@vps610e70>
2014-08-28 15:09:12 +0000

    first prototype of the website  ;)

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+error_reporting(E_ALL);
+$path = '/var/www/www.commonaccord.org/Alpha/';
+
+if(!isset($_REQUEST['action'])) { $_REQUEST['action'] = "list"; }
+
+if(isset($_REQUEST['file'])) {
+	$dir = $_REQUEST['file'];
+} else {
+	$dir = 'Doc/'; 
+}
+
+if($_REQUEST['action'] == 'list') {
+
+echo "Current directory = $dir<br>";
+$files = scandir($dir);
+
+
+
+foreach($files as $f) {
+	if(is_dir($path.$dir.$f)) {
+		echo "<a href=$_SERVER[PHP_SELF]?action=list&file=$dir$f/>$f</a><br>";
+	}
+	else {
+		echo "<a href=$_SERVER[PHP_SELF]?action=file&file=$dir$f>$f</a><br>";
+	}
+}
+
+} // end 'list'
+
+else if($_REQUEST['action'] == 'file') {
+
+$content = file_get_contents($dir, FILE_USE_INCLUDE_PATH);
+
+$contents = explode("\n", $content);
+
+$rootdir = pathinfo($dir);
+
+echo "
+<a href=$_SERVER[PHP_SELF]?action=list&file=$rootdir[dirname]/>$rootdir[dirname]</a><br>
+$dir<br>
+
+<h1>Source --> <a href=$_SERVER[PHP_SELF]?action=render&file=$dir>Render</a></h1>
+<table border=1>
+";
+foreach($contents as $n) {
+	list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+	echo "<tr>";
+	if(isset($k)) { echo "<td>$k</td><td>$v</td>"; }
+	else { echo "$k"; }
+	echo "</tr>";
+}
+
+echo "</table>";
+} // end 'file'
+
+else if($_REQUEST['action'] == 'render') {
+
+system("perl parser2.pl $path/$dir  > $path/Output999.html");
+echo file_get_contents("Output999.html", FILE_USE_INCLUDE_PATH);
+
+
+
+} // end 'render'
+
+else if($_REQUEST['action'] == 'pull') {
+
+echo `cd /var/www/www.commonaccord.org/Alpha; git pull -f 2>&1`;
+
+}
+
+
+?>
