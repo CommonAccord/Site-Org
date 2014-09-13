630d0c5dd74aa16ba552c006518224c0d561bcac
James Hazard <james.g.hazard@gmail.com>
2014-09-13 12:06:40 -0400

    adding descriptions

--- Doc/core/include.php
+++ Doc/core/include.php
@@ -1,23 +1,26 @@
 <h1> Overview of CommonAccord </h1>
 
 <table>
+
 <tr>
-<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/form/>form</a></td><td><img src="https://www.manilla.com/wp-content/uploads/2011/06/ph-paperstack.jpg" height="100px"></td><td>explanation...</td>
+<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/id/>id</a></td><td><img src="http://cdns2.freepik.com/free-photo/group--persons-outline--ios-7-interface-symbol_318-35219.jpg" height="100px"></td><td>People.  People are the fundamental unit.  They have relationships, own things, are places.</td>
+</tr>
+
+<tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/ids/>ids</a></td><td><img src="https://cdn0.iconfinder.com/data/icons/seo-smart-pack/128/grey_new_seo2-31-512.png" height="100px"></td><td>ID<u>s</u> are combinations of persons.  Parties to a contract.  Students at a school.  Business partners, employees, board members, agents.  Service providers, regulators, friends with whom you share something.</td></tr>
+
+
+<tr>
+   <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/form/>form</a></td><td><img src="https://www.manilla.com/wp-content/uploads/2011/06/ph-paperstack.jpg" height="100px"></td><td>Forms are legal documents and their components.  Contracts, permits, pleadings.  The text that describes the relationship and transaction.  The part of the legal system that touches persons.</td>
 </tr>
 
 <tr>
-<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/law/>law</a></td><td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="100px"></td><td>explanation...</td>
+   <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/law/>law</a></td><td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="100px"></td><td>Is agency-created forms, codes, regulations, and laws.  It is closely aligned with at/ and /form.</td>
 </tr>
 
-<tr>
-<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/id/>id</a></td><td><img src="http://cdns2.freepik.com/free-photo/group--persons-outline--ios-7-interface-symbol_318-35219.jpg" height="100px"></td><td>explanation...</td>
-</tr>
 
-<tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/ids/>ids</a></td><td><img src="https://cdn0.iconfinder.com/data/icons/seo-smart-pack/128/grey_new_seo2-31-512.png" height="100px"></td><td>explanation...</td></tr>
+   <tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/re/>re</a></td><td><img src="http://be-my-guest.com/static/img/things-to-do-icons.png" height="100px"></td><td>Is things.  Mostly things that a person can own or sell, license, borrow or otherwise trade in.  /auto, /ip/patent/, /real/, etc.  These need to be tied with registration systems such as deed registries, DMVs, patent offices, etc.</td></tr>
 
-<tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/re/>re</a></td><td><img src="http://be-my-guest.com/static/img/things-to-do-icons.png" height="100px"></td><td>explanation...</td></tr>
-
-<tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/at/>at</a></td><td><img src="http://images.clipartpanda.com/world-map-clip-art-worldmap.gif" height="100px"></td><td>explanation...</td></tr>
+<tr><td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=<?=$dir?>/at/>at</a></td><td><img src="http://images.clipartpanda.com/world-map-clip-art-worldmap.gif" height="100px"></td><td>Is for places.   1 Broadway, Cambridge MA, USA becomes usa/ma/middlesex/cambridge/broadway/1/geo</td></tr>
 
 </table>
 
