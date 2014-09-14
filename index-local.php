b5107e2f1980f287a5a63e06be9b317e0db1d0e1
root <root@vps610e70>
2014-09-14 05:26:14 +0000

    iaoeua

--- index-local.php
+++ index-local.php
@@ -46,7 +46,7 @@ foreach($files as $f) {
 	}
 	else {
 		if( !( ($f == 'include.php') || preg_match('/^\./', $f) ) ) {
-			echo "<br><a href=$_SERVER[PHP_SELF]?action=source&file=$dir$f/>$f</a>";
+			echo "<br><a href=$_SERVER[PHP_SELF]?action=source&file=$dir$f>$f</a>";
 		}
 	}
 }
