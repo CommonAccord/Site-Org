bc26b7e16510fb04302e10740cc6e50f075a58a7
James Hazard <james.g.hazard@gmail.com>
2014-09-14 11:51:41 -0400

    giving names to "Bus." part of the Rules

--- Doc/include.php
+++ Doc/include.php
@@ -1,2 +1,2 @@
-<h5><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/about/>To get started see about</a>.</h5>
+<a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/about/>To get started see about</a>.
 
