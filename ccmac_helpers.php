96d08945c3d80c7360fb52766b51f6fa1b91bcb2
Stephen Palley <Wigmore@Stephens-MacBook-Pro.local>
2014-09-16 12:00:04 -0400

    heroku path

--- ccmac_helpers.php
+++ ccmac_helpers.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 // commonaccord.com $path = '/var/www/www.commonaccord.org/'; //
-$path = './' //h eroku path //
+$path = './'; //h eroku path //
 
 
 if(!isset($_REQUEST['action'])) { 
