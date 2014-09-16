a867dc058fd0705e5fae1268b548b11a7cbeb634
James Hazard <james.g.hazard@gmail.com>
2014-09-16 15:02:53 -0400

    cleaning up text

--- Doc/about/include.php
+++ Doc/about/include.php
@@ -1,44 +1,44 @@
 <div class ="col-sm-12">
 <table class="responsive">
-<h3 style="text-align:center">Legal relationships have structure.<br>Refactored documents can make the relationships clear, compact and actionable.<br>Codified by lawyers and secured in personal data stores.</h3>
+   <h3>Legal relationships have structure.<br>Refactored documents make them clear, compact and actionable.<br>Secured in personal data stores, codified by lawyers.</h3>
 <hr>
 <table class="table table-striped table-condensed table-bordered" style="padding:20px">
 
 <tr>
-<td><img src="http://cdns2.freepik.com/free-photo/group--persons-outline--ios-7-interface-symbol_318-35219.jpg" height="70px"></td>
-<td width="40px"><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/id/>id</a></td>
+<td><img src="http://cdns2.freepik.com/free-photo/group--persons-outline--ios-7-interface-symbol_318-35219.jpg" height="50px"></td>
+<td width="30p"><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/id/>id</a></td>
 <td>People come first.  They have relationships, own things, are places.</td>
 </tr>
 
 <tr>
-<td><img src="https://cdn0.iconfinder.com/data/icons/seo-smart-pack/128/grey_new_seo2-31-512.png" height="70px"></td>
+<td><img src="https://cdn0.iconfinder.com/data/icons/seo-smart-pack/128/grey_new_seo2-31-512.png" height="50px"></td>
 <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/ids/>ids</a></td>
-<td>ID<u>s</u> are combinations of persons.  Parties to a contract.  Students at a school.  Business partners, employees, board members, agents.  Service providers, regulators, friends with whom you share something.</td></tr>
+   <td>ID<u>s</u> are combinations of persons.  Parties to a contract.  Students at a school.  Business partners, employees, board members, agents, subsidiaries.  Service providers, regulators, friends with whom you share something.</td></tr>
 
 
 <tr>
-<td><img src="https://www.manilla.com/wp-content/uploads/2011/06/ph-paperstack.jpg" height="70px"></td>
-<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/form/>form</a></td><td>Forms are legal documents and their components.  Contracts, permits, pleadings.  The text that describes the relationship and transaction.  The part of the legal system that touches persons.</td>
+<td><img src="https://www.manilla.com/wp-content/uploads/2011/06/ph-paperstack.jpg" height="50px"></td>
+<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/form/>form</a></td><td>Forms are legal documents and their components.  Contracts, permits, pleadings.  Text that describes relationships and transactions.</td>
 </tr>
 
 <tr>
-<td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="70px"></td>
-<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/law/>law</a></td>
-<td>Is agency-created forms, codes, regulations, and laws.  It is closely aligned with at/ and /form.</td>
+<td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="50px"></td>
+<td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/gov/>gov</a></td>
+   <td>For agency-created forms, the part that people touch.  Tied to codes, regulations and laws.  Closely aligned with at/ and /form.</td>
 </tr>
 
 
 <tr>
-<td><img src="http://be-my-guest.com/static/img/things-to-do-icons.png" height="70px"></td>
+<td><img src="http://be-my-guest.com/static/img/things-to-do-icons.png" height="50px"></td>
 <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/re/>re</a></td>
 <td>Is things.  Mostly things that a person can own or sell, license, borrow or otherwise trade in.  /auto, /ip/patent/, /real/, etc.  These need to be tied with registration systems such as deed registries, DMVs, patent offices, etc.</td></tr>
 
 <tr>
-<td><img src="http://images.clipartpanda.com/world-map-clip-art-worldmap.gif" height="70px"></td>
+<td><img src="http://images.clipartpanda.com/world-map-clip-art-worldmap.gif" height="50px"></td>
 <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/at/>at</a></td>
 <td>Is for places.   1 Broadway, Cambridge MA, USA becomes at/usa/ma/middlesex/cambridge/broadway/1/geo.</td></tr>
 <tr>
-<td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="70px"></td>
+<td><img src="http://www.lakelocal.org/forms/PublishingImages/forms-icon.jpg"height="50px"></td>
 <td><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/event/>event</a></td>
    <td>Events are transactions that connect people.  Making an application, granting a permit, signing a deal, sending a draft.  Each is a connection between some persons, a form and a history. History is the chain succession of deals.</td>
 </tr>
