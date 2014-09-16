200454a4307ef0252018da9ba5ad54615f45198e
root <root@vps610e70>
2014-09-16 13:46:52 +0000

    ifxed the problem with path for custom.css

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width">
-<link  href="/assets/reset.css" rel="stylesheet" />
+<link  href="./assets/reset.css" rel="stylesheet" />
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />
-<link  href="/assets/custom.css" rel="stylesheet" />
+<link  href="./assets/custom.css" rel="stylesheet" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="/assets/print.js"></script>
-<script src="/assets/save_my_div.js"></script>
+<script src="./assets/print.js"></script>
+<script src="./assets/save_my_div.js"></script>
 </head>
 <html>
 
