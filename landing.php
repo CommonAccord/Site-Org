b49ed02eb926cdd068f5ba44a1940a547f16ba5e
root <root@vps610e70>
2014-09-17 14:37:26 +0000

    added explore

--- landing.php
+++ landing.php
@@ -13,11 +13,10 @@
 
 <!--<a id="button-try" href="#"><img src="assets/tryit.png" height=150> </a> -->
 <div id="info-more">
-<h4 class="subtitle">Understand <a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/about/>the big picture.</a></h4>
+<h4 class="subtitle">Explore <a href=<?=$_SERVER['PHP_SELF']?>?action=explore>the template system.</a></h4>
 <h4 class="subtitle">Learn <a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/about/how/>how it works</a>.</h4>
 <h4 class="subtitle">See <a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/event/>some deals</a>.</h4>
-<h4 class="subtitle">Go to <a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=Doc/core/>the core</a>.</h4>
-<h4 class="subtitle">Browse <a href="https://github.com/CommonAccord/commonaccord/tree/master/Doc">the repo</a>.</h4>
+<h4 class="subtitle">Browse <a href="https://github.com/CommonAccord/commonaccord/tree/master/Doc">the Github repo</a>.</h4>
 
 <br/><a href="#more"><img src="assets/more.png" height=75></a>
 <br/>
