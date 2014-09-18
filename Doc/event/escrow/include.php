45025843fbd30187186a6a0cfbf4225fcd60392a
root <root@vps610e70>
2014-09-18 11:43:14 +0000

    added the include.php for the doc/event/escrw

---
+++ Doc/event/escrow/include.php
@@ -0,0 +1,11 @@
+<div class="row">
+<div class="col-md-4"><img height=400 src="assets/escrow.png"></div>
+<div class="col-md-6"> 
+
+We are presenting here the example of an escrow system, subdivided into a step-by-step process:<br>
+<ol>
+	<li> Parties agrees on the "Template Form" for the escrow agreement</li>
+	<li> Paries sign the agreement</li>
+</ol>
+ </div>
+</div>
