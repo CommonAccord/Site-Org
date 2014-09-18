dc9782fa3d42f27693515a7c15e34b218bca372b
root <root@vps610e70>
2014-09-18 12:42:29 +0000

    icolors..

--- Doc/event/escrow/include.php
+++ Doc/event/escrow/include.php
@@ -1,13 +1,12 @@
 <div class="row">
-<div class="col-md-4"><img height=400 src="assets/escrow.png"></div>
+<div class="col-md-4"><img height=300 src="assets/escrow.png"></div>
 <div class="col-md-6"> 
 
-   For example, cards for an escrow transaction, step-by-step:<br>
+We are presenting here the example of an escrow system, subdivided into a step-by-step process:<br>
 <ol>
-	<li> Parties are linked to the template for the escrow agreement</li>
-	<li> The Purchaser orders the goods</li>
-   <li> Parties sign the agreement (would actually be one-step per signature)</li>	<li> The Purchaser makes a complaint and proposes a remedy</li>
-  
+	<li> Buyer <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/roberta_robinson">Roberta Robinson</a> wants to purchase a product from Seller <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/gerrys_grapes_llc"> Gerry's Grapes</a></li>
+	<li>Parties agrees on the <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/Form">Template Agreement</a> and <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/JAMS_Arbitration_Chicago">Arbitration Rules</a> for the transaction</li>
+	<li> Paries sign the agreement</li>
 </ol>
  </div>
 </div>
