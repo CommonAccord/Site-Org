1827421fd9b82c172354d8590f6e9d7592241129
root <root@vps610e70>
2014-09-18 20:08:41 +0000

    fixed the dupilcate in the missing paratemers

--- parser2.pl
+++ parser2.pl
@@ -76,9 +76,12 @@ sub expand_fields  {
 
 my $output  = parse($ARGV[0], "Model.Root");
 print $output;
+
+# XXX FIX ME XXX This is horrible - but  I'm just dead tired  :(
 print "<br><br><hr><br><br>";
 print "<div id='missing'><p><h3 class='subtitle2'>Missing parameters:</h3>";
-my @arr = $output=~/\{([^}]+)\}/g;
+my %seen; my @arr = $output=~/\{([^}]+)\}/g;
+@arr = grep { ! $seen{$_}++ } @arr;
 print "$_=<br>" foreach @arr;
 print "</p></div>";
 
