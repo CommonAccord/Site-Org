ffffb5f6e67a9df8c615e1cd77d05d1d0ea1890a
James Hazard <james.g.hazard@gmail.com>
2014-09-18 08:30:16 -0400

    escrow

--- Doc/event/escrow/include.php
+++ Doc/event/escrow/include.php
@@ -2,10 +2,12 @@
 <div class="col-md-4"><img height=400 src="assets/escrow.png"></div>
 <div class="col-md-6"> 
 
-We are presenting here the example of an escrow system, subdivided into a step-by-step process:<br>
+   For example, cards for an escrow transaction, step-by-step:<br>
 <ol>
-	<li> Parties agrees on the "Template Form" for the escrow agreement</li>
-	<li> Paries sign the agreement</li>
+	<li> Parties are linked to the template for the escrow agreement</li>
+	<li> The Purchaser orders the goods</li>
+   <li> Parties sign the agreement (would actually be one-step per signature)</li>	<li> The Purchaser makes a complaint and proposes a remedy</li>
+  
 </ol>
  </div>
 </div>
