93cb5b2f5f06f67bcfe41277939abcf6df54c55c
James Hazard <james.g.hazard@gmail.com>
2014-09-26 07:13:34 -0400

    moving Roberta to her order

--- Doc/doc/escrow/include.php
+++ Doc/doc/escrow/include.php
@@ -4,11 +4,9 @@
 
 An escrow to assure that the online seller of goods gets paid only after the purchaser has received the goods and has had a certain time to inspect them.  Subdivided into four steps:<br>
 <ol>
-	<li> Purchaser <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/roberta_robinson">Roberta Robinson</a> wants to purchase a product from Seller <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/gerrys_grapes_llc"> Gerry's Grapes</a></li>
-	<li>Parties agree on the <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/Form">Template Agreement</a> and <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/JAMS_Arbitration_Chicago">Arbitration Rules</a> for the transaction</li>
-	<li> Paries introduce their names as Parameters into the Template agreement and get a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_1_Parties">new version</a> of the Agreement (step 1)</li>
-	<li> Roberta specifies the goods, creating an <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_2_Order">Order</a> that is a second version</a> of the Agreement (step 2)</li>
-	<li> Parties sign the contract and get a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_3_Signature">final version</a> of the Agreement (step 3)</li>
+	<li> Seller <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/gerrys_grapes_llc"> Gerry's Grapes</a> uses a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/Form">Template Agreement</a> and <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/form/agt/escrow/JAMS_Arbitration_Chicago">Arbitration Rules</a> in a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_1_Parties">form Agreement</a> (step 1)</li>
+	<li>Purchaser <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/core/id/roberta_robinson">Roberta Robinson</a> adds her name and specifies the goods, creating an <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_2_Order">Order</a> that is a second version of the Agreement (step 2)</li>
+	<li>The parties sign the contract and get a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_3_Signature">final version</a> of the Agreement (step 3)</li>
 <li>There is a problem with the goods and Roberta files a <a href="http://www.commonaccord.org/index.php?action=source&file=Doc/doc/escrow/Demo_Escrow_Step_4_Claim">claim (step 4)</a></li> 
 </ol>
  </div>
