597e9fa36895d07e2227f059b2b3dbc5cc2b472a
James Hazard <james.g.hazard@gmail.com>
2014-09-26 09:02:16 -0400

    eliminating "event"

--- Doc/event/include.php
+++
@@ -1,3 +0,0 @@
-"Event" is being "deprecated."  It is a lousy word for a good idea.  Each "event" in law is a document.  "Document" speaks more directly to the point.  So, see doc/.  Keeping existing materials here for a little while bz people have some links here.  But go see doc/.    
-
-			
