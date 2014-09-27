654ea99c7b55ca6ab5fc192b6330b9dd97965e50
James Hazard <james.g.hazard@gmail.com>
2014-09-27 11:55:51 -0400

    additional examples

---
+++ Doc/doc/medical/include.php
@@ -0,0 +1,12 @@
+These show a medical record release request.  
+
+The form is:
+
+core/form/policy/health/privacyNYP_Patient_Authorization
+
+In the first v01 we bring together parties.  Go there and look at the "Render" tab, then go to the bottom and you will see a list of "missing" fields.  To make the full document, we copy that list of missing fields and paste it in to v02.
+
+In the second v02 we make a complete record. 
+
+In the third v03, we take advantage of the structure to eliminate the unneeded MCUs (medical correspondence units) at the bottom of the document. 
+
