0243704d24006ac5a92a53ba63198fc57213d622
James Hazard <james.g.hazard@gmail.com>
2014-09-27 21:26:22 -0400

    example of ppt

--- Doc/doc/medical/include.php
+++ Doc/doc/medical/include.php
@@ -9,4 +9,5 @@ In the first v01 we bring together parties.  Go there and look at the "Render" t
 In the second v02 we make a complete record. 
 
 In the third v03, we take advantage of the structure to eliminate the unneeded MCUs (medical correspondence units) at the bottom of the document. 
-
+<br><br>
+  Also did two privacy statements based on the NYP privacy brochure.  The automation includes HHS Office for Civil Rights (core/id/gov_hhs_ocr).  They are part of the graph and could be a source for more of the solution.
