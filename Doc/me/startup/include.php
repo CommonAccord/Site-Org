6d1bad8ec6546cfeac3f59e2d29414fcf84c951d
James Hazard <james.g.hazard@gmail.com>
2014-10-05 16:20:42 -0400

    moving

---
+++ Doc/me/startup/include.php
@@ -0,0 +1,3 @@
+A sketch of the needs of a startup.   Drawing documents from a number of sources.  Note that none of these, so far, are endorsed or supported by the sources.  If you are a source, or have another solution, please feel free to claim the source or contribute the solution.  If you have a pain, please let the community know.  <a href='http://twitter.com/commonaccord'>@commonaccord</a>.  We'll refactor this to reflect the ideas of the Company in a Box from the "Code the Deal" hackathon (<a href='http://www.commonaccord.org/index.php?action=list&file=Doc/me/Hackcme/'>Acme/</a>).
+
+
