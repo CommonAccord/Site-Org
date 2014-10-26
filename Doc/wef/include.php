36ada7751eadde2b04506f02f60bbf3abbfeda18
James Hazard <james.g.hazard@gmail.com>
2014-10-26 11:01:31 -0400

    simplifying the intro

--- Doc/wef/include.php
+++ Doc/wef/include.php
@@ -1,17 +1,17 @@
-WE REPEAT:<br><b>NO MENTION OF ANY PERSON HERE SHOULD BE UNDERSTOOD TO INDICATE ANY APPROVAL, NOR THE TRUTH OF ANY STATEMENT OR RELATIONSHIP.  SOME PUBLIC FIGURES ARE MENTIONED AND SOME OF THE INFORMATION ABOUT THEM IS DRAWN FROM PUBLIC RECORDS.  BUT OTHER PARTS ARE INVENTED.  DO NOT TREAT ANY OF THIS INFORMATION AS TRUE.</b><br><br>
 
+<ol><li>
+<a href="http://commonaccord.org/index.php?action=list&file=Doc/wef/mta/">Materials Transfer Agreements</a>  
+</li><li>
+   <a href="http://commonaccord.org/index.php?action=list&file=Doc/wef/letter/us_tx_dshs/">Texas Department of State Health Services - Order, Contact, Temperature Reading</a><br>
+</li><li>
+<a href="http://commonaccord.org/index.php?action=source&file=Doc/wef/ebola/form/US/CDC/hcp/infection-prevention-and-control-recommendations">CDC Infection Prevention and Control Recommendations</a> 
+</li><li>
+   <a href="http://commonaccord.org/index.php?action=list&file=Doc/doc/roi/steps/">Sketch of a system of codified privacy for medical records (with PPR)<br>
+</a>
+</li><li>
+<a href="http://commonaccord.org/index.php?action=list&file=Doc/doc/master/">A Master Services Agreement that includes provisions regarding the transfer of personal information.<br>
+</a><br>
+</li></ol>
 
-A set of examples of the chain of text - from policy through contract to the actual medical record.  Indicative, not integrated.  <br>
+<b>NO MENTION OF ANY PERSON HERE SHOULD BE UNDERSTOOD TO INDICATE ANY APPROVAL, NOR THE TRUTH OF ANY STATEMENT OR RELATIONSHIP.  SOME PUBLIC FIGURES ARE MENTIONED AND SOME OF THE INFORMATION ABOUT THEM IS DRAWN FROM PUBLIC RECORDS.  BUT OTHER PARTS ARE INVENTED.  DO NOT TREAT ANY OF THIS INFORMATION AS TRUE.</b><br><br>
 
-Texas Department of State Health Services - Order, Signature by a HCW, and three temperature readings:
-<a href="http://commonaccord.org/index.php?action=list&file=Doc/wef/letter/us_tx_dshs/">wef/letter/us_tx_dshs/</a><br>
-
-Texas Health Presbyterian Hospital Dallas - Press Release:
-<a href="http://commonaccord.org/index.php?action=source&file=Doc/wef/ebola/texas_health_dallas/PressRelease_2014-10-16">texas_health_dallas/PressRelease_2014-10-16</a>  <br>
-
-The CDC:
-<a href="http://commonaccord.org/index.php?action=source&file=Doc/wef/ebola/form/US/CDC/hcp/infection-prevention-and-control-recommendations">form/US/CDC/hcp/infection-prevention-and-control-recommendations</a> <br>
-  See also:<br> the example of a patient interaction with privacy policies and an authorization app, at 
-<a href="http://commonaccord.org/index.php?action=list&file=Doc/doc/roi/steps/">doc/roi/steps/</a><br>
-  A Master Services Agreement that includes provisions regarding the transfer of personal information.  This could be build out, and include "break the glass" provisions.<br>
-<a href="http://commonaccord.org/index.php?action=list&file=Doc/doc/master/">doc/master/</a><br>
