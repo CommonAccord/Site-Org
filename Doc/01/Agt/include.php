cec0534c6f95578478312bd7af3ec124b1141573
James Hazard <james.g.hazard@gmail.com>
2014-11-06 10:22:02 -0500

    01/ frame - coordinating with Mediawiki

---
+++ Doc/01/Agt/include.php
@@ -0,0 +1,11 @@
+01/Agt/Frame = The basic framework of an agreement
+<br>
+
+01/Agt/Phrase/ = Phrases used in agreements
+<br>
+01/Agt/Sec/ =  Sections
+<br>
+
+
+01/Agt/Type/ = Specific types of agreements
+
---
+++ Doc/01/include.php
@@ -0,0 +1,3 @@
+This 01/ index of documents is intended for systemic approach to documents.  
+
+It can be coordinated with the Mediawiki version at <a href="http://response.commonaccord.org/01/">response.commonaccord.org/01/</a>
