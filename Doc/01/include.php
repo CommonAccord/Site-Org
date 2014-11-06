05e4716842bd25a8123e0daeaa9bc4e9d092eb55
James Hazard <james.g.hazard@gmail.com>
2014-11-06 10:29:59 -0500

    better english

--- Doc/01/include.php
+++ Doc/01/include.php
@@ -1,3 +1,3 @@
-This 01/ index of documents is intended for systemic approach to documents.  
+This 01/ is a systematic approach to documents, beginning with contracts.  
 
 It can be coordinated with the Mediawiki version at <a href="http://response.commonaccord.org/01/">response.commonaccord.org/01/</a>
