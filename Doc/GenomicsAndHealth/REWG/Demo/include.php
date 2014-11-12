7cfbe99d5740a40a479d12ebe9ffea379624e552
James Hazard <james.g.hazard@gmail.com>
2014-11-12 09:01:50 -0500

    outline better

--- Doc/GenomicsAndHealth/REWG/Demo/include.php
+++ Doc/GenomicsAndHealth/REWG/Demo/include.php
@@ -15,11 +15,11 @@ This example illustrates the use of CommonAccord for the creation of Consent For
 <li><a href="http://commonaccord.org/index.php?action=source&file=GenomicsAndHealth/REWG/Demo/Study_N01">The Study Framework</a></li>
 
 <li>Consents<ul>
-<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study">Consent_to_Study</a>: A master Consent Form for the Study </li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study">Consent_to_Study</a>: A master Consent Form for the Study </li><ul>
 <li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_US">Consent_to_Study_US</a>: A US-based instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Roberta_Robinson_US">signed version</a></li>
 <li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_FR">Consent_to_Study_FR</a>: A French instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Quentin_Quade_FR">signed version</a></li><ul>
    <li>The French version, <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Quentin_Quade_FR_Modified">presented with the signature section first.</a></li></ul>
-<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_DE">Consent_to_Study_DE</a>: A German instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Geraldine_Graber_DE">signed version</a></li></ul>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_DE">Consent_to_Study_DE</a>: A German instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Geraldine_Graber_DE">signed version</a></li></ul></ul>
 <li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Study_Signatures">A Listing of the Signatures from Each Donor</a></li></li>
    
 </ul>
