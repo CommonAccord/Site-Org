2421eaa0f8af332c225e73f5bedfa26c7b86cb62
root <root@vps610e70>
2014-11-12 06:26:05 +0000

    fixed the text in the include.php

--- Doc/GenomicsAndHealth/REWG/Demo/include.php
+++ Doc/GenomicsAndHealth/REWG/Demo/include.php
@@ -1,4 +1,4 @@
 <div class="row">
 <img src="./Doc/GenomicsAndHealth/REWG/Demo/cmacc-consent.png"><br>
-This example illustrate the use of CommonAccord for the creation of Consent Forms, which can inherit different clauses according to the language or jurisdiction that needs to be used.
+This example illustrate the use of CommonAccord for the creation of Consent Forms, which can inherit different clauses according to the language or jurisdiction that needs to be used. Click on the following links to see how it works:
 </div>
