ced1ac860e7541ffcadee1d021263777d3e6256a
root <root@vps610e70>
2014-11-12 06:51:18 +0000

    more sohpsticated include.php

--- Doc/GenomicsAndHealth/REWG/Demo/include.php
+++ Doc/GenomicsAndHealth/REWG/Demo/include.php
@@ -1,4 +1,19 @@
+<style>
+.listings { display: none;
+}
+</style>
+
 <div class="row">
+This example illustrate the use of CommonAccord for the creation of Consent Forms, which can inherit different clauses according to the language or jurisdiction that needs to be used. Switch between the different tabs (Source, Render, or Edit) to see how it works:
+<ul>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Consent/Outline_V01">Outline_V01</a>: Organise the taxonomy of the template Consent Forms</li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Consent/US/Form_V01">Form_US</a>: An US-based instantiation of the Consent Form</li>
+<li><a href="http://commonaccord.org/index.php?action=list&file=./GenomicsAndHealth/REWG/Consent/FR/">Form_FR</a>: A French instantiation of the Consent Form</li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Consent/DE/Form_V01">Form_DE</a>: A German instantiation of the Consent Form</li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study">Consent_to_Study</a>A specific Consent Form, that refers to the particular <a href="">Study</a></li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_US">Consent_to_Study_US</a>: A US-based instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Roberta_Robinson_US">signed version</a></li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_FR">Consent_to_Study_FR</a>: A French instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Quentin_Quade_FR">signed version</a></li>
+<li><a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Consent_To_Study_FR">Consent_to_Study_DE</a>: A German instantiation of the specific Consent Form, and its <a href="http://commonaccord.org/index.php?action=source&file=./GenomicsAndHealth/REWG/Demo/Geraldine_Graber_DE">signed version</a></li>
+</ul>
 <img src="./Doc/GenomicsAndHealth/REWG/Demo/cmacc-consent.png"><br>
-This example illustrate the use of CommonAccord for the creation of Consent Forms, which can inherit different clauses according to the language or jurisdiction that needs to be used. Click on the following links to see how it works:
 </div>
