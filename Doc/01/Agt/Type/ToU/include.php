fe04ea4ee513097fa6042489d692fb3139fffaf5
James Hazard <james.g.hazard@gmail.com>
2014-11-17 21:46:23 -0500

    example

---
+++ Doc/01/Agt/Type/ToU/include.php
@@ -0,0 +1,3 @@
+This Terms of Use document is quite comprehensive and can be forked for most uses.  It is borrowed from Google's cloud services, notably for genome storage. 
+<br>
+For an example of use - see <a href="http://www.commonaccord.org/index.php?action=source&file=./doc/web/Acme_ToU_Demo_1">/doc/web/Acme_ToU_Demo_1</a>
