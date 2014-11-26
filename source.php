eada8c9e170fb7160feab0bace2f3baa446dbddf
James Hazard <james.g.hazard@gmail.com>
2014-11-26 11:41:09 -0500

    link to github version

--- source.php
+++ source.php
@@ -3,8 +3,8 @@
 
 //This displays the path, current file name, and provides the edit and show options //
 
-echo "<a href=$_SERVER[PHP_SELF]?action=list&file=$rootdir[dirname]/>Org$rootdir[dirname]</a>/
-<b>$filenameX</b>
+echo "<a href=$_SERVER[PHP_SELF]?action=list&file=$rootdir[dirname]/>Org$rootdir[dirname]</a>/<b>$filenameX</b>
+&emsp; &emsp; &emsp; &emsp;  <a href=https://github.com/CommonAccord/commonaccord/blob/master/Doc/$rootdir[dirname]/$filenameX>GitHub</a>
 <br><br>
 ";
 ?>
