318591ce0aa2789f0f247cdbcd2e551b47295e76
James Hazard <james.g.hazard@gmail.com>
2014-11-29 14:39:47 -0500

    simpler menubar

--- menubar.php
+++ menubar.php
@@ -2,12 +2,12 @@
       <a class="navbar-brand" href="http://commonaccord.org"><img src="assets/cmacc-w.png" height=25></a>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-  	<li class="active"><a href="http://commonaccord.org/index.php?action=list&file=/">Home</a></p></li>
+  	<li class="active"><a href="/index.php?action=list&file=/">Docs</a></p></li>
         <li class="active"><a href="mailto:contact@commonaccord.org">Contact</a></li>
   
   		<li class="inactive"><a href="#">Terms of Service</a> </li>
   	<li class="inactive"><a href="#">Privacy Policy</a></li>
-  	<li><a href="http://commonaccord.org/index.php?action=print&file=me/cc/license/4_0/BY">Copyright</a></p></li>
+  	<li><a href="/index.php?action=print&file=me/cc/license/4_0/BY">Copyright</a></p></li>
   		<li class="inactive"><a href="#"> CommonAccord 2014</a></li>
       </ul>
     </div><!--/.nav-collapse -->
