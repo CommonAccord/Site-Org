195f1ea478cdae5ff306c8b864ec8737f3553fc8
James Hazard <james.g.hazard@gmail.com>
2014-12-13 11:37:16 -0500

    warning - not legal advice

---
+++ Doc/CooleyGo/include.php
@@ -0,0 +1,4 @@
+Used with the kind permission of <a href='http://cooley.com'>Cooley LLP </a>.
+
+This is text, not legal advice.  Please DO NOT rely, either on their originals or on our playing around with them.  Seek legal advice.  Send pull requests. 
+
