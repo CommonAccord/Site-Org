9e3ee70332c3f77de5f45e86d8bd8510b41c3333
James Hazard <james.g.hazard@gmail.com>
2014-12-26 07:57:25 +0100

    Privacy Policy
    From Tim Moynihan model.  Further automated and many minor drafting
    changes.

--- Doc/02/include.php
+++ Doc/02/include.php
@@ -1,1 +1,1 @@
-This NDA originates in Cooley GO.  Let us expand it and make it the only NDA you'll ever have to read. Overview at <a href="https://github.com/CommonAccord/Org/wiki/Jam%3A-NDA">GitHub/wiki/Jam: NDA</a>
+This NDA and Consulting Agreement originate in Cooley GO (with permission).  Let us expand it and make it the only ones you'll ever have to read. Overview at <a href="https://github.com/CommonAccord/Org/wiki/Jam%3A-NDA">GitHub/wiki/Jam: NDA</a>
