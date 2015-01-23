6bc2abb4dc7b2b1afc7d28c4cdad722069e2f79b
James Hazard <james.g.hazard@gmail.com>
2015-01-23 14:42:27 -0500

    explain

---
+++ Doc/03/Form/include.php
@@ -0,0 +1,3 @@
+Most recent materials are in this /03/: <br>A few examples of latest materials from #DecentLaw.  <br>
+
+The flagship NDA.  NDA is a common starting point for relationships.  It is confidentiality on top of a stack of "agreement" - representations, dispute resolution, miscellaneous terms.  See <a href="http://commonaccord.org/index.php?action=list&file=02/Demo/NDA/">NDA examples</a>.
