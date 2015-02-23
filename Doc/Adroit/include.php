794e8e1a5b1f9addafbff75d76270eb08585beab
James Hazard <james.g.hazard@gmail.com>
2015-02-23 10:34:39 -0800

    New /04/ as collection of current work.

--- Doc/Adroit/include.php
+++ Doc/Adroit/include.php
@@ -1,1 +1,1 @@
-These are a few documents contributed by <a href="http://adroitlawyers.com.au">Adroit Lawyers</a> as an experiment.  They are not legal advice. <br>See the two demo docs.  Find the marmots. 
+These are a Confidentiality "Deed" and Employment "Deed" under Australian law contributed by <a href="http://adroitlawyers.com.au">Adroit Lawyers</a>.  The equivalent of an NDA and Employment Agreement.  They are not legal advice. <br>See the two demo docs.  Marmots included. 
