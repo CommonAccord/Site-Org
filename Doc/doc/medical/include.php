27e20b01bc0ed745161905f36cdeac6382b556ff
James Hazard <james.g.hazard@gmail.com>
2015-07-02 20:32:47 +0200

    better text for include

--- Doc/doc/medical/include.php
+++ Doc/doc/medical/include.php
@@ -2,12 +2,12 @@ These show a medical record release request.
 
 The form is:
 
-core/form/policy/health/privacyNYP_Patient_Authorization
-
+core/form/policy/health/privacyNYP_Patient_Authorization.
+<br>
 In the first v01 we bring together parties.  Go there and look at the "Render" tab, then go to the bottom and you will see a list of "missing" fields.  To make the full document, we copy that list of missing fields and paste it in to v02.
-
+<br>
 In the second v02 we make a complete record. 
-
+<br>
 In the third v03, we take advantage of the structure to eliminate the unneeded MCUs (medical correspondence units) at the bottom of the document. 
 <br><br>
   Also did two privacy statements based on the NYP privacy brochure.  The automation includes HHS Office for Civil Rights (core/id/gov_hhs_ocr).  They are part of the graph and could be a source for more of the solution.
