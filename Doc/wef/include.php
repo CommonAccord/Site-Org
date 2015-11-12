5214efda9bb24c115e16137d3154f86081c2af85
HazardJ <jamesghazard@gmail.com>
2015-11-13 00:40:18 +0100

    Update include.php

--- Doc/wef/include.php
+++ Doc/wef/include.php
@@ -2,19 +2,19 @@ Materials for World Economic Forum - Data-Driven Development - initiative on eme
 
 
 <ol><li>
-<a href="http://commonaccord.org/index.php?action=list&file=wef/mta/">Standard Materials Transfer Agreements</a>  
+<a href="/index.php?action=list&file=wef/mta/">Standard Materials Transfer Agreements</a>  
 </li><li>
 
-<a href="http://commonaccord.org/index.php?action=list&file=wef/gsk/mta/">GSK Open Innovation Challenge - Model MTA</a>  
+<a href="/index.php?action=list&file=wef/gsk/mta/">GSK Open Innovation Challenge - Model MTA</a>  
 </li><li>
-   <a href="http://commonaccord.org/index.php?action=list&file=wef/letter/us_tx_dshs/">Texas Department of State Health Services - Order, Contact, Temperature Reading</a><br>
+   <a href="/index.php?action=list&file=wef/letter/us_tx_dshs/">Texas Department of State Health Services - Order, Contact, Temperature Reading</a><br>
 </li><li>
-<a href="http://commonaccord.org/index.php?action=source&file=wef/ebola/form/US/CDC/hcp/infection-prevention-and-control-recommendations">CDC Infection Prevention and Control Recommendations</a> 
+<a href="/index.php?action=source&file=wef/ebola/form/US/CDC/hcp/infection-prevention-and-control-recommendations">CDC Infection Prevention and Control Recommendations</a> 
 </li><li>
-   <a href="http://commonaccord.org/index.php?action=list&file=doc/roi/steps/">Sketch of a system of codified privacy for medical records (with PPR)<br>
+   <a href="/index.php?action=list&file=doc/roi/steps/">Sketch of a system of codified privacy for medical records (with PPR)<br>
 </a>
 </li><li>
-<a href="http://commonaccord.org/index.php?action=list&file=doc/master/">A Master Services Agreement that includes provisions regarding the transfer of personal information.<br>
+<a href="/index.php?action=list&file=doc/master/">A Master Services Agreement that includes provisions regarding the transfer of personal information.<br>
 </a><br>
 </li></ol>
 
